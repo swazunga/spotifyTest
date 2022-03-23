3a007041e854854565df0709bbf7cf42ac11c7a4
Michael Kendrick <michael@locumsmart.net>
2022-03-22 23:22:14 -0500

    details, pic, and music

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+var button = document.querySelector(".button");
+var playBtn = document.querySelector(".play");
+var detailsBtn = document.querySelector(".details");
+var apiUrl =
+  "https://freesound.org/apiv2/sounds/560446/?token=WQG0rpvXpgS6UJ0x1cigSmQIh0rpUThXm0PTZ6ea";
+var picUrl =
+  "https://freesound.org/apiv2/users/Migfus20/?token=WQG0rpvXpgS6UJ0x1cigSmQIh0rpUThXm0PTZ6ea";
+var playUrl =
+  "https://freesound.org/apiv2/sounds/560446/?token=WQG0rpvXpgS6UJ0x1cigSmQIh0rpUThXm0PTZ6ea";
+var apiKey = "WQG0rpvXpgS6UJ0x1cigSmQIh0rpUThXm0PTZ6ea";
+
+function apiFunc() {
+  console.log("sup");
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  console.log("Hi");
+
+  var audio = fetch();
+}
+
+function getPic() {
+  console.log("pic");
+  fetch(picUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var list = document.querySelector(".list");
+      var pic = document.createElement("img");
+      pic.src = data.avatar.large;
+      list.append(pic);
+      getDets();
+    });
+}
+
+function getDets() {
+  console.log("the dets");
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var list = document.querySelector(".list");
+      var fact = document.createElement("li");
+      var fact2 = document.createElement("li");
+      var fact3 = document.createElement("li");
+      fact.textContent = data.username;
+      fact2.textContent = data.name;
+      fact3.textContent = data.tags[0];
+      list.append(fact);
+      list.append(fact2);
+      list.append(fact3);
+    });
+}
+
+function playMusic() {
+  console.log("play");
+  fetch(playUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.previews["preview-hq-mp3"]);
+      var music = new Audio(data.previews["preview-hq-mp3"]);
+      music.volume = 1;
+      music.play();
+    });
+}
+
+button.addEventListener("click", apiFunc);
+detailsBtn.addEventListener("click", getPic);
+playBtn.addEventListener("click", playMusic);
