c24116585a2e6cc246fc3a9174ce746e507ed166
Michael Kendrick <michael@locumsmart.net>
2022-03-23 09:05:48 -0500

    adding similar music

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var button = document.querySelector(".button");
 var playBtn = document.querySelector(".play");
 var detailsBtn = document.querySelector(".details");
+var userUrl = "https://freesound.org/apiv2/users/";
 var apiUrl =
   "https://freesound.org/apiv2/sounds/560446/?token=WQG0rpvXpgS6UJ0x1cigSmQIh0rpUThXm0PTZ6ea";
 var picUrl =
@@ -52,6 +53,7 @@ function getDets() {
       list.append(fact);
       list.append(fact2);
       list.append(fact3);
+      similarSounds();
     });
 }
 
@@ -67,6 +69,48 @@ function playMusic() {
       var music = new Audio(data.previews["preview-hq-mp3"]);
       music.volume = 1;
       music.play();
+    });
+}
+
+function similarSounds() {
+  console.log("similar");
+  fetch(playUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.similar_sounds);
+      fetch(data.similar_sounds + "?token=" + apiKey)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          for (var i = 1; i < 5; i++) {
+            fetch(userUrl + data.results[i].username + "/?token=" + apiKey)
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (data) {
+                var list = document.querySelector(".list");
+                var pic = document.createElement("img");
+                var picSrc = data.avatar.large;
+                pic.src = picSrc;
+                list.append(pic);
+              });
+            var pic = document.createElement("img");
+            var list = document.querySelector(".list");
+            var fact = document.createElement("li");
+            var fact2 = document.createElement("li");
+            var fact3 = document.createElement("li");
+            fact.textContent = data.results[i].username;
+            fact2.textContent = data.results[i].name;
+            fact3.textContent = data.results[i].tags[0];
+            list.append(fact);
+            list.append(fact2);
+            list.append(fact3);
+          }
+        });
     });
 }
 
